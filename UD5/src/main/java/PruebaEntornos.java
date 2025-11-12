53073eca6c01d6935d7b9b649cbec7509ddba269
jorpasmon <jorpasmon@alu.edu.gva.es>
2025-11-12 10:24:59 +0100

    Rama

--- UD5/src/main/java/PruebaEntornos.java
+++ UD5/src/main/java/PruebaEntornos.java
@@ -7,7 +7,8 @@ public class PruebaEntornos {
         System.out.print("Introduce tu año de nacimiento:");
         int anyo = sc.nextInt();
         LocalDateTime fechaActual = LocalDateTime.now();
-        int edad = fechaActual.getYear() + anyo;
+        int edad = fechaActual.getYear() - anyo;
+        System.out.println("---------------------------");
         if (edad > 18) {
             System.out.println("Eres mayor de edad");
         } else {
