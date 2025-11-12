55c261667de485fcf7632735c10fb5c269607623
J3Plu1009mnb <migmarsan3@alu.edu.gva.es>
2025-11-12 11:19:51 +0100

    Cambio de Miguel

--- UD5/src/main/java/PruebaEntornos.java
+++ UD5/src/main/java/PruebaEntornos.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 
 public class PruebaEntornos {
     public static void main(String[] args) {
+
+        final int anio = 2025;
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduce tu año de nacimiento:");
         int anyo = sc.nextInt();
-        LocalDateTime fechaActual = LocalDateTime.now();
-        int edad = fechaActual.getYear() + anyo;
+
+        int edad = anio - anyo;
+        System.out.println("Año actual: " + anio);
         if (edad > 18) {
             System.out.println("Eres mayor de edad");
         } else {
